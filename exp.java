a1e0e7e4a25d515d04e9b74974d8ae7011d76c7b
manaskg <manaskumarghosh91@gmail.com>
2024-07-31 20:40:38 +0530

    new file added

---
+++ exp.java
@@ -0,0 +1,28 @@
+class Student1
+{
+    private int age;
+    private String name;
+    public void setData()
+    {
+        age = 18;
+        name = "Manas";
+    }
+    public void show()
+    {
+        System.out.println(name + " " + age);
+    }
+
+    public void show()
+    {
+        System.out.println(name + " " + age);
+    }
+}
+public class Encapsulationn {
+    public static void main(String[] args) 
+    {
+        Student1 obj = new Student1();
+        Student1 obj1 = new Student1();
+        obj.setData();
+        obj.show();
+    }
+}
